d60c972b35e761770ffa659405e0c3b6619b3767
natchanon pornprasatpol <natchanon.por@cpf.co.th>
2023-09-06 17:19:32 +0700

    up

--- src/helper.test.ts
+++ src/helper.test.ts
@@ -1,11 +1,21 @@
-import { add } from "./helper";
+import { add, subtrac } from "./helper";
 
 test("test add function", () => {
-  //   //arrange
-  //   const a = 1;
-  //   const b = 2;
-  //   //act
-  //   const actual = add(1, 2);
-  //   //assert
-  //   expect(actual).toEqual(3);
+  //arrange
+  const a = 1;
+  const b = 2;
+  //act
+  const actual = add(a, b);
+  //assert
+  expect(actual).toEqual(3);
+});
+
+test("test subtrack function", () => {
+  //arrange
+  const a = 2;
+  const b = 1;
+  //act
+  const actual = subtrac(a, b);
+  //assert
+  expect(actual).toEqual(1);
 });
--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,7 @@
 export function add(a: number, b: number) {
   return a + b;
 }
+
+export function subtrac(a: number, b: number) {
+  return a - b;
+}
